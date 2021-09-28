ae636d3492cfb19bf8068805e2b7ca18f5ad4d9f
KouOsada <kouosada.0930@gmail.com>
2021-09-28 09:25:27 +0000

    [Add]

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,16 @@
 
 var title = "javascriptが使えました";
 alert(title)
+
+//= require jquery
+
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+$(document).ready(function () {
+  $('.jquery').on('click', function(){
+    $(this).css('color','red');
+  });
+});
